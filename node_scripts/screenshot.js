1885cf80d755bccb7375a8223c9260c19af2edb2
dandrecollera <dandrecollera@gmail.com>
2024-02-28 07:39:40 +0800

    test log

--- node_scripts/screenshot.js
+++ node_scripts/screenshot.js
@@ -1,16 +1,31 @@
 const puppeteer = require("puppeteer");
 
 async function takeScreenshot(url, path) {
-    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-    const page = await browser.newPage();
-
-    await page.setViewport({ width: 1366, height: 768 });
-
-    await page.goto(url, { waitUntil: "networkidle2" });
-    await page.screenshot({ path: path });
-    await browser.close();
+    try {
+        const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1366, height: 768 });
+        await page.goto(url, { waitUntil: "networkidle2" });
+        await page.screenshot({ path: path });
+        await browser.close();
+        return true; // Return true if screenshot is taken successfully
+    } catch (error) {
+        console.error("Error while taking screenshot:", error);
+        return false; // Return false if an error occurs
+    }
 }
 
 const url = process.argv[2];
 const path = process.argv[3];
-takeScreenshot(url, path).then(() => console.log("Screenshot taken for", url));
+
+takeScreenshot(url, path)
+    .then((success) => {
+        if (success) {
+            console.log("Screenshot taken for", url);
+        } else {
+            console.error("Failed to take screenshot for", url);
+        }
+    })
+    .catch((error) => {
+        console.error("Error occurred:", error);
+    });
